5a239abd9ee9a022b1b3ea3d67136024e33a8cb3
Sandeep Kushaj Vithanage <sandeepkushaj3@gmail.com>
2022-04-12 13:30:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     SECRET_KEY: "0000-0000-0000-0000",
-    MONGO_DB_URL: "mongodb+srv://sandeep:Abcde12345@sandeep.vt4f0.mongodb.net/testnodejs?retryWrites=true&w=majority"
+    MONGO_DB_URL: "mongodb+srv://..."
 };
